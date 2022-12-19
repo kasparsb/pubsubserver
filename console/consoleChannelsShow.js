2b1ee4db23a61fda22d6797dcf4bae9ce53fd173
Kaspars Bulins <kasparsb@hotmail.com>
2022-12-19 03:23:16 +0000

    Channels show izvada subscriberNotify pazimes

--- console/consoleChannelsShow.js
+++ console/consoleChannelsShow.js
@@ -6,12 +6,15 @@ function consoleChannelsShow(args, done) {
     Channels.loadFromDb(function(){
 
         Channels.getChannels().forEach(channel => {
-            console.log(
-                channel.dbid+' | '+
-                channel.id+' | '+
-                channel.notifyEndpoints.subscriberStatusChange.join('; ')+' | '+
-                channel.notifyEndpoints.subscriberMessageRecieved.join('; ')
-            )
+            console.log(channel.name+' ('+channel.id+')');
+            console.log('listenerNotifyEndpoints.subscriberStatusChange');
+            channel.listenerNotifyEndpoints.subscriberStatusChange.forEach(url => console.log('    '+url));
+            console.log('listenerNotifyEndpoints.subscriberMessageRecieved');
+            channel.listenerNotifyEndpoints.subscriberMessageRecieved.forEach(url => console.log('    '+url));
+            console.log('subscriberNotify.subscriberStatusChange '+(channel.subscriberNotify.subscriberStatusChange ? 'true' : 'false'));
+            console.log('subscriberNotify.subscriberMessageRecieved '+(channel.subscriberNotify.subscriberMessageRecieved ? 'true' : 'false'));
+
+            console.log('--------------------------------------------------');
         })
 
         done();
