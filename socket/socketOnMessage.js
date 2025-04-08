34dd2616009c9d91ecaf2a882b38297690f46831
Kaspars Bulins <kasparsb@hotmail.com>
2025-04-08 15:55:13 +0000

    Izkomenteta veca funkcionalitate, kuru laikam nevajadzes

--- socket/socketOnMessage.js
+++ socket/socketOnMessage.js
@@ -1,5 +1,4 @@
 let Channels = require('../Channels');
-let ClientsList = require('../ClientsList');
 let Listeners = require('../Listeners');
 let timer = require('../timer');
 
@@ -32,51 +31,56 @@ function socketOnMessage(client, message) {
         console.log(e);
     }
 
-    if (data.type == 'ping') {
-        /**
-         * Ja connections uzrādās, ka disconnected, tad nesūtām pong
-         * Ir mirkļi, kad connection uzrādās kā false, bet klienta puse mierīgi
-         * iesūta datus un arī saņem pretī pong. Tāpēc šeit, ja ir disconnected,
-         * fakit, lai klienta puse slēdzas atkārtoti
-         */
-        //if (client.connection.connected) {  Patestējam, kā būs ja sūtīs ping arī tam, kas nav connected
-            // Atbildam ar pong
+    switch (data.type) {
+        case 'ping':
+            /**
+             * Ja connections uzrādās, ka disconnected, tad nesūtām pong
+             * Ir mirkļi, kad connection uzrādās kā false, bet klienta puse mierīgi
+             * iesūta datus un arī saņem pretī pong. Tāpēc šeit, ja ir disconnected,
+             * fakit, lai klienta puse slēdzas atkārtoti
+             */
+            //if (client.connection.connected) {  Patestējam, kā būs ja sūtīs ping arī tam, kas nav connected
+
             client.sendMessage(messagePong());
             client.touchPongAt();
-            //ClientsList.setSubscriberStatusPong(subscriber);
-        //}
+
+            break;
+
+        case 'subscribe-to-topics':
+
+            if (typeof data.topics != 'undefined') {
+                Channels.subscribeClientToTopics(client, data.topics);
+            }
+
+            break;
+    }
 
 
-    }
-    // Pierakstās uz topic
-    else if (data.type == 'subscribe-topics') {
-        if (typeof data.topics != 'undefined') {
-            Channels.subscribeClientToTopics(client, data.topics);
-        }
-    }
-    /**
-     * Special case, admin channel subscribers
-     * they can watch changes in
-     *      channelChange
-     *      subscriberChange
-     * @eventName channelChange|subscriberChange
-     * @subjects channel name or subscriber id
-     */
-    else if (data.type == 'watch') {
-        Listeners.add(subscriber, data.eventName, data.subjects);
-    }
-    else if (data.type == 'unwatch') {
-        Listeners.remove(subscriber, data.eventName, data.subjects);
-    }
-    else if (data.type == 'unwatchall') {
-        Listeners.remove(subscriber);
-    }
-    else if (data.type == 'message') {
-        Channels.notifySubscriberMessageRecieved(
-            // Channel name
-            client.channel,
-            messageMessage(data.message, undefined, client)
-        );
-    }
+
+
+    // /**
+    //  * Special case, admin channel subscribers
+    //  * they can watch changes in
+    //  *      channelChange
+    //  *      subscriberChange
+    //  * @eventName channelChange|subscriberChange
+    //  * @subjects channel name or subscriber id
+    //  */
+    // else if (data.type == 'watch') {
+    //     Listeners.add(subscriber, data.eventName, data.subjects);
+    // }
+    // else if (data.type == 'unwatch') {
+    //     Listeners.remove(subscriber, data.eventName, data.subjects);
+    // }
+    // else if (data.type == 'unwatchall') {
+    //     Listeners.remove(subscriber);
+    // }
+    // else if (data.type == 'message') {
+    //     Channels.notifySubscriberMessageRecieved(
+    //         // Channel name
+    //         client.channel,
+    //         messageMessage(data.message, undefined, client)
+    //     );
+    // }
 }
 module.exports = socketOnMessage;
